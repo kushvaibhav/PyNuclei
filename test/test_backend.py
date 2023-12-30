fee82d89dfe3873532eecfa2456954469a199413
Noam Rathaus <rathaus@gmail.com>
2023-12-30 19:34:45 +0200

    Check the mterics backend

--- test/test_backend.py
+++ test/test_backend.py
@@ -10,6 +10,6 @@ nucleiScanner = PyNuclei.Nuclei()
 # http://honey.scanme.sh is a specially made host by Nuclei team to test the setups
 scanResult = nucleiScanner.scan(
     "http://honey.scanme.sh", templates=["cves", "network", "ssl"], rateLimit=150,
-    verbose=True
+    verbose=True, metrics=True
 )
 print(scanResult)
