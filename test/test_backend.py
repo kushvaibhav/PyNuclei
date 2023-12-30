988dc3e2099b1952c4b320753f341b5e524f81bb
Noam Rathaus <rathaus@gmail.com>
2023-12-30 19:20:29 +0200

    Improved test - make it more verbose

--- test/test_backend.py
+++ test/test_backend.py
@@ -9,6 +9,7 @@ import PyNuclei
 nucleiScanner = PyNuclei.Nuclei()
 # http://honey.scanme.sh is a specially made host by Nuclei team to test the setups
 scanResult = nucleiScanner.scan(
-    "http://honey.scanme.sh", templates=["cves", "network", "ssl"], rateLimit=150
+    "http://honey.scanme.sh", templates=["cves", "network", "ssl"], rateLimit=150,
+    verbose=True
 )
 print(scanResult)
