3b67f924dd5a65778b255c80928b197951da91d1
Noam Rathaus <noamr@ssd-disclosure.com>
2023-12-31 09:48:59 +0200

    param name change

--- test/test_backend.py
+++ test/test_backend.py
@@ -9,7 +9,10 @@ import PyNuclei
 nucleiScanner = PyNuclei.Nuclei()
 # http://honey.scanme.sh is a specially made host by Nuclei team to test the setups
 scanResult = nucleiScanner.scan(
-    "http://honey.scanme.sh", templates=["cves", "network", "ssl"], rateLimit=150,
-    verbose=True, metrics=True
+    "http://honey.scanme.sh",
+    templates=["cves", "network", "ssl"],
+    rate_limit=150,
+    verbose=True,
+    metrics=True,
 )
 print(scanResult)
