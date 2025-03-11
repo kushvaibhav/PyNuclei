e36a743c6e673ecb5105d25222c18bd117f31a85
Vaibhav Kushwaha <vaibhavkush.007@gmail.com>
2025-03-11 19:48:19 +0530

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PyNuclei",
-    version="1.4",
+    version="1.4.1",
     author="Vaibhav Kushwaha",
     author_email="vaibhavkush.007@gmail.com",
     description="PyNuclei is an unofficial python library for Nuclei Scanner.",
